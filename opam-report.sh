4904f2fe5a73aa5f2a877f72d64f80167134e92f
Blue Prawn <monnier.florent@gmail.com>
2013-09-01 17:34:52 +0200

    also provide the error log files

--- opam-report.sh
+++ opam-report.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-export OPAMROOT="/mnt/ramdisk/opam-report/.opam"
+OS="mga"
+
+### XXX be very carefull: rm -rf is arround!
+BASEDIR="/tmp/opam-report"
+#BASEDIR="/mnt/ramdisk/opam-report"
+
+export OPAMROOT="$BASEDIR/.opam"
+
+LOGDIR="$BASEDIR/logs-$OS-`date --iso`"
+mkdir -p $LOGDIR
 
 rm -rf $OPAMROOT
 echo n | opam init
 eval `opam config env`
 
 PKG_LIST=`opam list --short`
-
 for PKG in $PKG_LIST
 do
   rm -rf $OPAMROOT
@@ -16,7 +24,17 @@ do
 
     echo ""
     echo "====== begin: $PKG"
-    opam install -y $PKG && echo "### OK for: $PKG" || echo "### FAIL for: $PKG"
+    opam install -y $PKG
+    if [ "$?" = "0" ]; then STATUS="YEAH"; else STATUS="ARGH"; fi
+    if [ "$STATUS" = "YEAH" ]; then echo "### OK for: $PKG"; fi
+    if [ "$STATUS" = "ARGH" ]; then echo "### FAIL for: $PKG"; fi
+    if [ "$STATUS" = "ARGH" ]; then
+        PKG_LOG_DIR="`basename $OPAMROOT/system/build/$PKG*`"
+        mkdir -p $LOGDIR/$PKG_LOG_DIR/
+        echo "### copy logs to: $PKG_LOG_DIR/"
+        cp $OPAMROOT/system/build/$PKG_LOG_DIR/$PKG-*.err $LOGDIR/$PKG_LOG_DIR/
+        cp $OPAMROOT/system/build/$PKG_LOG_DIR/$PKG-*.out $LOGDIR/$PKG_LOG_DIR/
+    fi
     opam remove $PKG
     echo "====== end: $PKG"
     echo ""
